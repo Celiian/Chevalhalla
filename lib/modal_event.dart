ddceadade28da51e03205dab69df2f03635813b4
maximeduyne <maximeduyne@icloud.com>
2022-11-17 14:26:13 +0100

    Creattion des modal et alert evenement

--- lib/modal_event.dart
+++ lib/modal_event.dart
@@ -1,40 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:intl/intl.dart';
 
-
-class ModalEvent{
-
+class ModalEvent {
   void modalChoice(BuildContext context, _dateController) {
     showModalBottomSheet(
         context: context,
         builder: (BuildContext bc) {
           return Padding(
-            padding: const EdgeInsets.only(top: 20.0,bottom: 20.0),
+            padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
             child: Wrap(
               children: <Widget>[
                 const Padding(
                   padding: EdgeInsets.only(left: 100.0),
-                  child: Text('Que voullez-vous crée ?', style: TextStyle(fontSize: 20), textAlign: TextAlign.center),
+                  child: Text('Que voullez-vous crée ?',
+                      style: TextStyle(fontSize: 20),
+                      textAlign: TextAlign.center),
                 ),
                 ListTile(
-                    title: const Text('Un Cours', textAlign: TextAlign.center,),
+                    title: const Text(
+                      'Un Cours',
+                      textAlign: TextAlign.center,
+                    ),
                     onTap: () => {
-                      Navigator.pop(context),
-                      //_modalCours(context),
-                    }),
+                          Navigator.pop(context),
+                          CreateCours(context, _dateController)
+                        }),
                 ListTile(
                   title: const Text('Un Concour', textAlign: TextAlign.center),
-                  onTap: () => {
-                    Navigator.pop(context),
-                    //_modalConcour(context),
-                  },
+                  onTap: () =>
+                      {Navigator.pop(context), CreateCompetition(context)},
                 ),
                 ListTile(
                   title: const Text('Une Soirée', textAlign: TextAlign.center),
-                  onTap: () =>
-                  {
-                  CreateSoiree(context, _dateController),
+                  onTap: () => {
+                    Navigator.pop(context),
+                    CreateSoiree(context, _dateController),
                   },
                 ),
               ],
@@ -42,7 +44,6 @@ class ModalEvent{
           );
         });
   }
-
 
   void CreateSoiree(BuildContext context, _dateController) {
     final format = DateFormat("HH:mm");
@@ -55,6 +56,21 @@ class ModalEvent{
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
+                  DropdownButtonFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Type de soirée',
+                      ),
+                      items: const [
+                        DropdownMenuItem(
+                          value: 'Apéro',
+                          child: Text('Apéro'),
+                        ),
+                        DropdownMenuItem(
+                          value: 'Repas',
+                          child: Text('Repas'),
+                        ),
+                      ],
+                      onChanged: (value) {}),
                   TextFormField(
                     //controller: ,
                     decoration: const InputDecoration(
@@ -70,37 +86,41 @@ class ModalEvent{
                   TextFormField(
                     controller: _dateController,
                     decoration: const InputDecoration(
-                      hintText: 'Entrez la date de la soirée',),
+                      hintText: 'Entrez la date de la soirée',
+                    ),
                     validator: (String? value) {
                       if (value == null || value.isEmpty) {
                         return 'Please enter some text';
                       }
                       return null;
                     },
-                    readOnly: true,  //set it true, so that user will not able to edit text
+                    readOnly: true,
+                    //set it true, so that user will not able to edit text
                     onTap: () async {
                       final DateTime? date = await showDatePicker(
                           context: context,
                           initialDate: DateTime.now(),
-                          firstDate: DateTime(2021),
-                          lastDate: DateTime(2100)
-                      );
+                          firstDate: DateTime.now(),
+                          lastDate: DateTime(2100));
                       if (date != null) {
-                        final String formattedDate = DateFormat('dd-MM-yyyy').format(date);
+                        final String formattedDate =
+                            DateFormat('dd-MM-yyyy').format(date);
                         _dateController.text = formattedDate;
                       }
                     },
                   ),
                   DateTimeField(
                     format: format,
-                    decoration:
-                    const InputDecoration(
-                      label: Text('Entrez la date de la soirée',),
+                    decoration: const InputDecoration(
+                      label: Text(
+                        'Entrez l\'heure de la soirée',
+                      ),
                     ),
                     onShowPicker: (context, currentValue) async {
                       final time = await showTimePicker(
                         context: context,
-                        initialTime: TimeOfDay.fromDateTime(currentValue ?? DateTime.now()),
+                        initialTime: TimeOfDay.fromDateTime(
+                            currentValue ?? DateTime.now()),
                       );
                       return DateTimeField.convert(time);
                     },
@@ -108,7 +128,198 @@ class ModalEvent{
                   TextFormField(
                     //controller: ,
                     decoration: const InputDecoration(
-                      hintText: 'Entrez le lieu de la soirée',),
+                      hintText: 'Description de la soirée',
+                    ),
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                  ),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Annuler'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Créer'),
+              ),
+            ],
+          );
+        });
+  }
+
+  void CreateCours(BuildContext context, _dateController) {
+    final format = DateFormat("HH:mm");
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Création d\'un cours'),
+            content: Form(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  DropdownButtonFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Discipline',
+                        hintText: 'Choisissez une discipline',
+                      ),
+                      items: const [
+                        DropdownMenuItem(
+                          value: 'Dressage',
+                          child: Text('Dressage'),
+                        ),
+                        DropdownMenuItem(
+                          value: 'Saut d’obstacle',
+                          child: Text('Saut d’obstacle'),
+                        ),
+                        DropdownMenuItem(
+                          value: 'Endurance',
+                          child: Text('Endurance'),
+                        ),
+                      ],
+                      onChanged: (value) {}),
+                  DropdownButtonFormField(
+                      decoration: const InputDecoration(
+                        hintText: 'Choisissez le terrain',
+                        labelText: 'Terrain',
+                      ),
+                      items: const [
+                        DropdownMenuItem(
+                          value: 'Carrière',
+                          child: Text('Carrière'),
+                        ),
+                        DropdownMenuItem(
+                          value: 'Manège',
+                          child: Text('Manège'),
+                        ),
+                      ],
+                      onChanged: (value) {}),
+                  TextFormField(
+                    //controller: ,
+                    decoration: const InputDecoration(
+                      labelText: 'Nom du cours',
+                      hintText: 'Entrez le nom du cours',
+                    ),
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                  ),
+                  DropdownButtonFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Durée du cours',
+                        hintText: 'Durée du cours',
+                      ),
+                      items: const [
+                        DropdownMenuItem(
+                          value: 60,
+                          child: Text('1h'),
+                        ),
+                        DropdownMenuItem(
+                          value: 30,
+                          child: Text('30min'),
+                        ),
+                      ],
+                      onChanged: (value) {}),
+                  TextFormField(
+                    controller: _dateController,
+                    decoration: const InputDecoration(
+                      labelText: 'Date du cours',
+                      hintText: 'Entrez la date du cours',
+                    ),
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                    readOnly: true,
+                    //set it true, so that user will not able to edit text
+                    onTap: () async {
+                      final DateTime? date = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime.now(),
+                          lastDate: DateTime(2100));
+                      if (date != null) {
+                        final String formattedDate =
+                            DateFormat('dd-MM-yyyy').format(date);
+                        _dateController.text = formattedDate;
+                      }
+                    },
+                  ),
+                  DateTimeField(
+                    format: format,
+                    decoration: const InputDecoration(
+                      labelText: 'Heure du cours',
+                      hintText: 'Entrez l\'heure du cours',
+                    ),
+                    onShowPicker: (context, currentValue) async {
+                      final time = await showTimePicker(
+                        context: context,
+                        initialTime: TimeOfDay.fromDateTime(
+                            currentValue ?? DateTime.now()),
+                      );
+                      return DateTimeField.convert(time);
+                    },
+                  ),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Annuler'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Créer'),
+              ),
+            ],
+          );
+        });
+  }
+
+  void CreateCompetition(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Création d\'une compétition'),
+            content: Form(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  TextFormField(
+                    //controller: ,
+                    decoration: const InputDecoration(
+                      labelText: 'Photo de la compétition',
+                      hintText: 'Entrez le lien de la photo',
+                    ),
+                  ),
+                  TextFormField(
+                    //controller: ,
+                    decoration: const InputDecoration(
+                      labelText: 'Nom de la compétition',
+                      hintText: 'Entrez le nom de la compétition',
+                    ),
                     validator: (String? value) {
                       if (value == null || value.isEmpty) {
                         return 'Please enter some text';
@@ -119,7 +330,9 @@ class ModalEvent{
                   TextFormField(
                     //controller: ,
                     decoration: const InputDecoration(
-                      hintText: 'Entrez le nombre de places',),
+                      labelText: 'Adresse de la compétition',
+                      hintText: 'Entrez l\'adresse de la compétition',
+                    ),
                     validator: (String? value) {
                       if (value == null || value.isEmpty) {
                         return 'Please enter some text';
@@ -127,6 +340,38 @@ class ModalEvent{
                       return null;
                     },
                   ),
+                  TextFormField(
+                    //controller: ,
+                    decoration: const InputDecoration(
+                      hintText: 'Entrez du concours',
+                    ),
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                    readOnly: true,
+                    //set it true, so that user will not able to edit text
+                    onTap: () async {
+                      final DateTime? date = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime.now(),
+                          lastDate: DateTime(2100));
+                      if (date != null) {
+                        final String formattedDate =
+                        DateFormat('dd-MM-yyyy').format(date);
+                        //_dateController.text = formattedDate;
+                      }
+                    },
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child:
+                      Text('Afficher les participants')),
                 ],
               ),
             ),
