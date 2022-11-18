7f4d14973c4da72b92d2a392d12fbb980e5d1ced
maximeduyne <maximeduyne@icloud.com>
2022-11-18 12:52:04 +0100

    Ajout du dialog des soirée

--- lib/pages/admin/list_party.dart
+++ lib/pages/admin/list_party.dart
@@ -43,11 +43,82 @@ class _ListPartyState extends State<ListParty> {
                   return ListTile(
                     title: Text(PartyList[index]['name']),
                     subtitle: Text(PartyList[index]['description']),
-
+                    onTap: () {
+                      _showDialog(PartyList[index], context);
+                    },
                   );
                 },
               )
                   : Text("Il n'y pas de Soirée de prévue ")),
         ));
   }
+}
+
+  _showDialog(soirees, context) {
+    var date = soirees["date"].toString().split(" ")[0];
+    date = "${date.split("-")[2]} / ${date.split("-")[1]} / ${date.split("-")[0]}";
+    showDialog(context: context,
+        builder: (BuildContext context) {
+      return AlertDialog(
+        content: Image.network(
+            soirees["image"], width: 100), actions: <Widget>[
+        Column(
+          children: [
+            Text("Type : " + soirees["type"],
+                style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold)),
+            Text("Date : " + date,
+                style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold)),
+            Text("Heure : " + soirees["hour"],
+                style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold)),
+            Text("Description : " + soirees["description"],
+                style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center),
+            Text("Crée par : " + soirees["user_name"],
+                style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold)),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 8.0, top: 30.0),
+              child: Row(
+                children: [
+                  TextButton(
+                      child: const Text("Refuser",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold)),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      }),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: TextButton(
+                      child: const Text("Valider",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold)),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        )
+      ],
+      );
+    });
+
 }
